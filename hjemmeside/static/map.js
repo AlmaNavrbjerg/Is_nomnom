8114c1ed870ba6e53f649d113fb618562a74a2e5
AlmaNavrbjerg <almasofie@navrbjerg.dk>
2026-05-28 15:13:30 +0200

    Update map.js

--- hjemmeside/static/map.js
+++ hjemmeside/static/map.js
@@ -7,9 +7,6 @@ const map = new maplibregl.Map({
 
 let marker = new maplibregl.Marker()
   .setLngLat([12.5670, 55.6732])
+  .setPopup(new maplibregl.Popup({ offset: 25 }) // create a popup with an offset
+    .setText('This is TIVOLI!')) // set the text for the popup
   .addTo(map); // add the marker to the map
-
-let popup = new maplibregl.Popup({ offset: 25 }) // offset er afstanden mellem popup og markøren
-  .setLngLat(marker.getLngLat()) // sæt popup'ens position til samme som markøren
-  .setText('This is a popup!') // set the text for the popup
-  .addTo(map); // add the popup to the map
