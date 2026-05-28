0194ffdfaeda071c0624c655b278a19d1fbc7fae
AlmaNavrbjerg <almasofie@navrbjerg.dk>
2026-05-28 15:03:50 +0200

    Marker

--- hjemmeside/static/map.js
+++ hjemmeside/static/map.js
@@ -4,3 +4,12 @@ const map = new maplibregl.Map({
     zoom: 11.3,
     container: 'map',
 });
+
+let marker = new maplibregl.Marker()
+  .setLngLat([12.5670, 55.6732])
+  .addTo(map); // add the marker to the map
+
+let popup = new maplibregl.Popup({ offset: 25 }) // offset er afstanden mellem popup og markøren
+  .setLngLat(marker.getLngLat()) // sæt popup'ens position til samme som markøren
+  .setText('This is a popup!') // set the text for the popup
+  .addTo(map); // add the popup to the map
